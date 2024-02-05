5aac89e49e9958972f0f426544a2cd3db1d943c3
Crowdbotics <team@crowdbotics.com>
2024-02-05 15:40:56 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "airbus-maintenance--45903",
+  url: "https://airbus-maintenance--45903.botics.co",
+  api: "https://airbus-maintenance--45903.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
